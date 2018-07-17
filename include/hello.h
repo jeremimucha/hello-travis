da0b09890a4188dd694845c9ab8c436e077d034c
Jeremi Mucha <rockpaperbanan@gmail.com>
2018-07-17 08:56:33 +0200

    first try

--- include/hello.h
+++ include/hello.h
@@ -1,3 +1,5 @@
 #pragma once
 
 int say_hello(const char*) noexcept;
+
+int say_hi(const char*) noexcept;
--- src/hello.cpp
+++ src/hello.cpp
@@ -6,3 +6,9 @@ int say_hello(const char* const name) noexcept
     std::printf("Hello, %s!", name);
     return 1;
 }
+
+int say_hi(const char* const name) noexcept
+{
+    std::printf("Hi, %s!", name);
+    return 1;
+}
